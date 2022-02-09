190b084733906d779e9ec8434537da68e0570a8a
Wat96 <wat9691@gmail.com>
2022-02-09 02:38:33 -0800

    checking in stuff

--- contracts/UniSwapArb.sol
+++ contracts/UniSwapArb.sol
@@ -1,15 +1,15 @@
 //SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
+pragma solidity =0.7.6;
+pragma abicoder v2;
 
 // Openzepplin
 import '@openzeppelin/contracts/access/Ownable.sol';
 
 // uniswap 2 imports
 import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Callee.sol';
+import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
 import '@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol';
 import '@uniswap/v2-periphery/contracts/interfaces/IUniswapV2Router02.sol';
-import '@uniswap/v2-periphery/contracts/interfaces/IERC20.sol';
-import '@uniswap/v2-periphery/contracts/interfaces/IWETH.sol';
 
 // uniswap 3 imports
 import '@uniswap/v3-core/contracts/interfaces/callback/IUniswapV3FlashCallback.sol';
@@ -29,19 +29,31 @@ contract PairFlash is IUniswapV2Callee, IUniswapV3FlashCallback, Ownable {
     // Should be set or checked before use
     mapping(address => bool) private whitelist;
     IUniswapV2Router02 public v2SwapRouter;
+    IUniswapV2Router02 public sushiSwapRouter;
+    IUniswapV2Router02 public otherV2Router;
     ISwapRouter public v3SwapRouter;
+    ISwapRouter public otherV3SwapRouter;
     address public v2Factory;
+    address public sushiFactory;
+    address public otherV2Factory;
     address public v3Factory;
+    address public otherV3Factory;
     address public WETH9;
 
     // Structs and enums
-    enum UniswapVersion { v2, v3 }
+    enum LPVersion { v2, oV2, v3, oV3, sushi }
 
     struct SwapInfo {
         address v2SwapRouter;
+        address sushiSwapRouter;
+        address otherV2Router;
         address v3SwapRouter;
+        address otherV3SwapRouter;
         address v2Factory;
+        address sushiFactory;
+        address otherV2Factory;
         address v3Factory;
+        address otherV3Factory;
         address WETH9;
     }
 
@@ -49,61 +61,39 @@ contract PairFlash is IUniswapV2Callee, IUniswapV3FlashCallback, Ownable {
         address token0;
         address token1;
         uint24 fee;
-        UniswapVersion version;
+        LPVersion version;
     }
 
-    // pool 0 is desc for flash pool
     struct FlashParams {
-        uint256 amount0;
-        uint256 amount1;
-        address token0;
-        address token1;
-        PoolDesc pool0;
-        PoolDesc pool1;
+        uint256 loanAmnt;
+        address loanToken;
+        PoolDesc loanPool;
+        PoolDesc[] poolPath;
     }
 
     struct FlashCallbackData {
-        uint256 amount0;
-        uint256 amount1;
+        uint256 loanAmnt;
+        address loanToken;
         address payer;
         PoolAddress.PoolKey poolKey;
-        uint24 poolFee2;
-        uint24 poolFee3;
+        PoolDesc[] poolPath;
     }
 
     // Some Utility Functions here
-    // pay function to make sending token easier
-    function pay (
-        address token,
-        address payer,
-        address recipient,
-        uint256 value
-    ) internal {
-        if (token == WETH9 && address(this).balance >= value) {
-            // pay with WETH9
-            IWETH9(WETH9).deposit{value: value}(); // wrap only what is needed to pay
-            IWETH9(WETH9).transfer(recipient, value);
-        } else if (payer == address(this)) {
-            // pay with tokens already in the contract (for the exact input multihop case)
-            TransferHelper.safeTransfer(token, recipient, value);
-        } else {
-            // pull payment
-            TransferHelper.safeTransferFrom(token, payer, recipient, value);
-        }
-    }
-
     function constructCallbackParams(
         FlashParams memory params
     ) internal returns (FlashCallbackData memory) {
         return FlashCallbackData({
-                    amount0: params.amount0,
-                    amount1: params.amount1,
-                    payer: msg.sender
+                    loanAmnt: params.loanAmnt,
+                    loanToken: params.loanToken,
+                    payer: msg.sender,
+                    poolPath: params.poolPath,
+                    poolKey: PoolAddress.PoolKey(address(0), address(0), 0)
                 });
     }
 
     function constructCallbackParams(
-        FlashParams memory params, 
+        FlashParams memory params,
         PoolAddress.PoolKey memory poolKey
     ) internal returns (FlashCallbackData memory) {
         FlashCallbackData memory res = constructCallbackParams(params);
@@ -111,13 +101,18 @@ contract PairFlash is IUniswapV2Callee, IUniswapV3FlashCallback, Ownable {
         return res;
     }
 
-
     // Access Control stuff here
     function setSwapInfo(SwapInfo memory swapInfo) external onlyOwner {
-        if (swapInfo.v2SwapRouter != address(0)) v2SwapRouter = swapInfo.v2SwapRouter;
-        if (swapInfo.v3SwapRouter != address(0)) v3SwapRouter = swapInfo.v3SwapRouter;
+        if (swapInfo.v2SwapRouter != address(0)) v2SwapRouter = IUniswapV2Router02(swapInfo.v2SwapRouter);
+        if (swapInfo.sushiSwapRouter != address(0)) sushiSwapRouter = IUniswapV2Router02(swapInfo.sushiSwapRouter);
+        if (swapInfo.otherV2Router != address(0)) otherV2Router = IUniswapV2Router02(swapInfo.otherV2Router);
+        if (swapInfo.v3SwapRouter != address(0)) v3SwapRouter = ISwapRouter(swapInfo.v3SwapRouter);
+        if (swapInfo.otherV3SwapRouter != address(0)) otherV3SwapRouter = ISwapRouter(swapInfo.otherV3SwapRouter);
         if (swapInfo.v2Factory != address(0)) v2Factory = swapInfo.v2Factory;
+        if (swapInfo.sushiFactory != address(0)) sushiFactory = swapInfo.sushiFactory;
+        if (swapInfo.otherV2Factory != address(0)) otherV2Factory = swapInfo.otherV2Factory;
         if (swapInfo.v3Factory != address(0)) v3Factory = swapInfo.v3Factory;
+        if (swapInfo.otherV3Factory != address(0)) otherV3Factory = swapInfo.otherV3Factory;
         if (swapInfo.WETH9 != address(0)) WETH9 = swapInfo.WETH9;
     }
 
@@ -133,9 +128,9 @@ contract PairFlash is IUniswapV2Callee, IUniswapV3FlashCallback, Ownable {
     // Entry functions here
     function startArb(FlashParams memory params) external onlyWhitelisted {
         // pool0 is flash pool, check what uniswap version
-        if (params.pool0.version == UniswapVersion.v2) {
+        if (params.loanPool.version == LPVersion.v2) {
             startArbV2(params);
-        } else if (params.pool0.version == UniswapVersion.v3) {
+        } else if (params.loanPool.version == LPVersion.v3) {
             startArbV3(params);
         }
 
@@ -143,36 +138,36 @@ contract PairFlash is IUniswapV2Callee, IUniswapV3FlashCallback, Ownable {
     }
 
     function startArbV2(FlashParams memory params) internal {
-        address token0 = params.pool0.token0;
-        address token1 = params.pool0.token1;
-        uint256 amount0 = params.amount0;
-        uint256 amount1 = params.amount1;
+        address token0 = params.loanPool.token0;
+        address token1 = params.loanPool.token1;
+        uint256 amount0 = params.loanToken == token0 ? params.loanAmnt : 0;
+        uint256 amount1 = params.loanToken == token1 ? params.loanAmnt : 0;
 
         // get pair addr and verify
-        address pairAddress = IUniswapV2Factory(v2Factory).getPair(token0, token1); 
-        require(pairAddress != address(0), 'Could not find pool on uniswap'); 
+        address pairAddress = IUniswapV2Factory(v2Factory).getPair(token0, token1);
+        require(pairAddress != address(0), 'Could not find pool on uniswap');
 
-        // create flashloan 
+        // create flashloan
         // bytes can not be empty or normal swap will happen
         IUniswapV2Pair(pairAddress).swap(
-            amount0, 
-            amount1, 
-            address(this), 
+            amount0,
+            amount1,
+            address(this),
             abi.encode(constructCallbackParams(params))
         );
     }
 
     function startArbV3(FlashParams memory params) internal {
-        address token0 = params.pool0.token0;
-        address token1 = params.pool0.token1;
-        uint256 amount0 = params.amount0;
-        uint256 amount1 = params.amount1;
-        uint24 fee = params.pool0.fee;
+        address token0 = params.loanPool.token0;
+        address token1 = params.loanPool.token1;
+        uint256 amount0 = params.loanToken == token0 ? params.loanAmnt : 0;
+        uint256 amount1 = params.loanToken == token1 ? params.loanAmnt : 0;
+        uint24 fee = params.loanPool.fee;
 
         // Get pool key
         PoolAddress.PoolKey memory poolKey =
             PoolAddress.PoolKey({token0: token0, token1: token1, fee: fee});
-        IUniswapV3Pool pool = IUniswapV3Pool(PoolAddress.computeAddress(factory, poolKey));
+        IUniswapV3Pool pool = IUniswapV3Pool(PoolAddress.computeAddress(v3Factory, poolKey));
 
         // initiate flash on pool
         pool.flash(
@@ -185,23 +180,152 @@ contract PairFlash is IUniswapV2Callee, IUniswapV3FlashCallback, Ownable {
 
     // Execute trade functions here:
     function executeSwap(
+        PoolDesc memory tradePool,
+        uint256 amountIn,
+        address tokenIn,
+        address tokenOut
+    ) internal returns (uint256) {
+        uint256 amountOut = 0;
+        if (tradePool.version == LPVersion.v2 || 
+            tradePool.version == LPVersion.sushi ||
+            tradePool.version == LPVersion.oV2) {
+            amountOut = executeSwapV2(tradePool, amountIn, tokenIn, tokenOut);
+        } else if (tradePool.version == LPVersion.v3 || tradePool.version == LPVersion.oV3) {
+            amountOut = executeSwapV3(tradePool, amountIn, tokenIn, tokenOut);
+        }
+        return amountOut;
+    }
 
-    ) internal {
+    function executeSwapV2(
+        PoolDesc memory tradePool,
+        uint256 amountIn,
+        address tokenIn,
+        address tokenOut
+    ) internal returns (uint256) {
+        address[] memory path = new address[](2);
+        path[0] = tokenIn;
+        path[1] = tokenOut;
 
+        IUniswapV2Router02 routerInUse;
+        address factoryInUse;
+        if (tradePool.version == LPVersion.v2) {
+            routerInUse = v2SwapRouter;
+            factoryInUse = v2Factory;
+        } else if (tradePool.version == LPVersion.sushi) {
+            routerInUse = sushiSwapRouter;
+            factoryInUse = sushiFactory;
+        } else if (tradePool.version == LPVersion.oV2) {
+            routerInUse = otherV2Router;
+            factoryInUse = otherV2Factory;
+        }
+
+        TransferHelper.safeApprove(tokenIn, address(routerInUse), amountIn);
+
+        // Calculate amount in and swap.
+        uint deadline = block.timestamp + 200;
+        uint amountRequired = UniswapV2Library.getAmountsOut(
+            factoryInUse, 
+            amountIn, 
+            path
+        )[0];
+        uint amountReceived = routerInUse.swapExactTokensForTokens(
+            amountIn, 
+            amountRequired, 
+            path, 
+            address(this),
+            deadline
+        )[1];
+
+        return amountReceived;
+    }
+
+    function executeSwapV3(
+        PoolDesc memory tradePool,
+        uint256 amountIn,
+        address tokenIn,
+        address tokenOut
+    ) internal returns (uint256) {
+        ISwapRouter routerInUse;
+        address factoryInUse;
+        if (tradePool.version == LPVersion.v3) {
+            routerInUse = v3SwapRouter;
+            factoryInUse = v3Factory;
+        } else if (tradePool.version == LPVersion.oV3) {
+            routerInUse = otherV3SwapRouter;
+            factoryInUse = otherV3Factory;
+        }
+
+        TransferHelper.safeApprove(tokenIn, address(routerInUse), amountIn);
+
+        uint256 amountOut =
+            routerInUse.exactInputSingle(
+                ISwapRouter.ExactInputSingleParams({
+                    tokenIn: tokenIn,
+                    tokenOut: tokenOut,
+                    fee: tradePool.fee,
+                    recipient: address(this),
+                    deadline: block.timestamp + 200,
+                    amountIn: amountIn,
+                    amountOutMinimum: 0,
+                    sqrtPriceLimitX96: 0
+                })
+            );
+        
+        return amountOut;
+    }
+
+    function executeTrades(FlashCallbackData memory params) internal returns (uint256, address) {
+        uint pathLen = params.poolPath.length;
+        uint256 amountToTrade = params.loanAmnt;
+        address tokenIn = params.loanToken;
+        address tokenOut = address(0);
+        for (uint256 i = 0; i < pathLen; ++i) {
+            PoolDesc memory poolDesc = params.poolPath[i];
+            tokenOut = poolDesc.token0 == tokenIn ? poolDesc.token1 : poolDesc.token0;
+            amountToTrade = executeSwap(poolDesc, amountToTrade, tokenIn, tokenOut);
+            tokenIn = tokenOut;
+        }  
+
+        return (amountToTrade, tokenOut);
     }
 
     // Callback functions start here:
     function uniswapV2Call(
-        address sender, 
-        uint amount0, 
-        uint amount1, 
+        address sender,
+        uint amount0,
+        uint amount1,
         bytes calldata data
-    ) external override { 
-        // Decode call data.
+    ) external override {
+        // Decode call data and verify calling pool
         FlashCallbackData memory decoded = abi.decode(data, (FlashCallbackData));
         address token0 = IUniswapV2Pair(msg.sender).token0();
         address token1 = IUniswapV2Pair(msg.sender).token1();
         assert(msg.sender == UniswapV2Library.pairFor(v2Factory, token0, token1));
+
+        // calc fee
+        uint fee = ((decoded.loanAmnt * 3) / 997) + 1;
+        uint amountOwed = decoded.loanAmnt + fee;
+
+        (uint256 amountOut, address tokenOut) = executeTrades(decoded);
+
+        // if token is diff, recalc amount to repay
+        if (tokenOut != decoded.loanToken) {
+            address[] memory path = new address[](2);
+            path[0] = tokenOut;
+            path[1] = decoded.loanToken;
+            amountOwed = UniswapV2Library.getAmountsIn(v2Factory, decoded.loanAmnt, path)[0];
+        }
+        // repay flash loan
+        TransferHelper.safeApprove(tokenOut, address(this), amountOwed);
+        TransferHelper.safeTransfer(tokenOut, decoded.payer, amountOwed);
+
+        // if profitable pay profits to payer
+        if (amountOut > amountOwed) {
+            uint256 profit = LowGasSafeMath.sub(amountOut, amountOwed);
+
+            TransferHelper.safeApprove(tokenOut, address(this), profit);
+            TransferHelper.safeTransfer(tokenOut, decoded.payer, profit);
+        }
     }
 
     function uniswapV3FlashCallback(
@@ -209,71 +333,25 @@ contract PairFlash is IUniswapV2Callee, IUniswapV3FlashCallback, Ownable {
         uint256 fee1,
         bytes calldata data
     ) external override {
+        // Decode call data and verify calling pool
         FlashCallbackData memory decoded = abi.decode(data, (FlashCallbackData));
         CallbackValidation.verifyCallback(v3Factory, decoded.poolKey);
 
-        address token0 = decoded.poolKey.token0;
-        address token1 = decoded.poolKey.token1;
-
-        TransferHelper.safeApprove(token0, address(swapRouter), decoded.amount0);
-        TransferHelper.safeApprove(token1, address(swapRouter), decoded.amount1);
-
-        // profitable check
-        // exactInputSingle will fail if this amount not met
-        uint256 amount1Min = LowGasSafeMath.add(decoded.amount1, fee1);
-        uint256 amount0Min = LowGasSafeMath.add(decoded.amount0, fee0);
-
-        // call exactInputSingle for swapping token1 for token0 in pool w/fee2
-        uint256 amountOut0 =
-            swapRouter.exactInputSingle(
-                ISwapRouter.ExactInputSingleParams({
-                    tokenIn: token1,
-                    tokenOut: token0,
-                    fee: decoded.poolFee2,
-                    recipient: address(this),
-                    deadline: block.timestamp + 200,
-                    amountIn: decoded.amount1,
-                    amountOutMinimum: amount0Min,
-                    sqrtPriceLimitX96: 0
-                })
-            );
-
-        // call exactInputSingle for swapping token0 for token 1 in pool w/fee3
-        uint256 amountOut1 =
-            swapRouter.exactInputSingle(
-                ISwapRouter.ExactInputSingleParams({
-                    tokenIn: token0,
-                    tokenOut: token1,
-                    fee: decoded.poolFee3,
-                    recipient: address(this),
-                    deadline: block.timestamp + 200,
-                    amountIn: decoded.amount0,
-                    amountOutMinimum: amount1Min,
-                    sqrtPriceLimitX96: 0
-                })
-            );
+        (uint256 amountOut, address tokenOut) = executeTrades(decoded);
+        require(tokenOut == decoded.loanToken);
 
         // end up with amountOut0 of token0 from first swap and amountOut1 of token1 from second swap
-        uint256 amount0Owed = LowGasSafeMath.add(decoded.amount0, fee0);
-        uint256 amount1Owed = LowGasSafeMath.add(decoded.amount1, fee1);
+        uint256 amountOwed = LowGasSafeMath.add(decoded.loanAmnt, LowGasSafeMath.add(fee0, fee1));
 
-        TransferHelper.safeApprove(token0, address(this), amount0Owed);
-        TransferHelper.safeApprove(token1, address(this), amount1Owed);
-
-        if (amount0Owed > 0) pay(token0, address(this), msg.sender, amount0Owed);
-        if (amount1Owed > 0) pay(token1, address(this), msg.sender, amount1Owed);
+        TransferHelper.safeApprove(decoded.loanToken, address(this), amountOwed);
+        TransferHelper.safeTransfer(decoded.loanToken, msg.sender, amountOwed);
 
         // if profitable pay profits to payer
-        if (amountOut0 > amount0Owed) {
-            uint256 profit0 = LowGasSafeMath.sub(amountOut0, amount0Owed);
+        if (amountOut > amountOwed) {
+            uint256 profit = LowGasSafeMath.sub(amountOut, amountOwed);
 
-            TransferHelper.safeApprove(token0, address(this), profit0);
-            pay(token0, address(this), decoded.payer, profit0);
-        }
-        if (amountOut1 > amount1Owed) {
-            uint256 profit1 = LowGasSafeMath.sub(amountOut1, amount1Owed);
-            TransferHelper.safeApprove(token0, address(this), profit1);
-            pay(token1, address(this), decoded.payer, profit1);
+            TransferHelper.safeApprove(decoded.loanToken, address(this), profit);
+            TransferHelper.safeTransfer(decoded.loanToken, decoded.payer, profit);
         }
     }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,55 @@
-require("@nomiclabs/hardhat-waffle");
-
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 /**
- * @type import('hardhat/config').HardhatUserConfig
- */
+    * @type import('hardhat/config').HardhatUserConfig
+*/
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config()
+
+const SOL_VERSION = process.env.SOL_VERSION || "0.8.3"
+const FORK_NODE_URL = process.env.FORK_NODE_URL
+const MAINNET_DEPLOY_PRIVATE_KEY = process.env.MAINNET_DEPLOY_PRIVATE_KEY
+const RINKEBY_DEPLOY_PRIVATE_KEY = process.env.RINKEBY_DEPLOY_PRIVATE_KEY
+const LOCALNET_DEPLOY_PRIVATE_KEY = process.env.LOCALNET_DEPLOY_PRIVATE_KEY
+const MAINNET_NODE_URL = process.env.MAINNET_NODE_URL
+const RINKEBY_NODE_URL = process.env.RINKEBY_NODE_URL
+
+let HARDHAT_CONFIG = {}
+let MAINNET_CONFIG = {}
+let RINKEBY_CONFIG = {}
+let NETWORKS_CONFIG = {}
+
+// configure hardhat
+if (LOCALNET_DEPLOY_PRIVATE_KEY) HARDHAT_CONFIG.accounts = [ LOCALNET_DEPLOY_PRIVATE_KEY ]
+if (FORK_NODE_URL) {
+    HARDHAT_CONFIG.forking = {
+        url: FORK_NODE_URL
+    }
+}
+NETWORKS_CONFIG.hardhat = HARDHAT_CONFIG
+
+// configure main net
+if (MAINNET_DEPLOY_PRIVATE_KEY && MAINNET_NODE_URL) {
+    MAINNET_CONFIG.accounts = [ MAINNET_DEPLOY_PRIVATE_KEY ]
+    MAINNET_CONFIG.url = MAINNET_NODE_URL
+    NETWORKS_CONFIG.mainnet = MAINNET_CONFIG
+}
+
+// configure rinkeby
+if (RINKEBY_DEPLOY_PRIVATE_KEY && RINKEBY_NODE_URL) {
+    RINKEBY_CONFIG.accounts = [ RINKEBY_DEPLOY_PRIVATE_KEY ]
+    RINKEBY_CONFIG.url = RINKEBY_NODE_URL
+    NETWORKS_CONFIG.rinkeby = RINKEBY_CONFIG
+}
+
+console.log(NETWORKS_CONFIG)
 module.exports = {
-  solidity: "0.8.4",
-};
+    solidity: {
+        version: SOL_VERSION,
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200
+            }
+        }
+    },
+    networks: NETWORKS_CONFIG
+}
